d161da007aa4bffd733d8ee4a4f4b44c997ebb56
Rhennan Carvalho <ascomfrc@gmail.com>
2026-05-06 00:12:04 -0300

    feat: adiciona menu principal com while e switch

--- main.ts
+++ main.ts
@@ -1,1 +1,38 @@
-console.log ('rodando...')
+import readline from 'readline'
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+function menu(): void {
+  console.log('\n=== TURMA CONTROL ===')
+  console.log('1 - Cadastrar aluno')
+  console.log('2 - Adicionar notas')
+  console.log('0 - Sair')
+  console.log('=====================')
+
+  rl.question('Escolha uma opção: ', (opcao) => {
+    let loop = true
+
+    switch (Number(opcao)) {
+      case 1:
+        console.log('>> Cadastrar aluno (em desenvolvimento)')
+        break
+      case 2:
+        console.log('>> Adicionar notas (em desenvolvimento)')
+        break
+      case 0:
+        loop = false
+        console.log('Encerrando o sistema...')
+        rl.close()
+        return
+      default:
+        console.log('Opção inválida!')
+    }
+
+    if (loop) menu()
+  })
+}
+
+menu()
